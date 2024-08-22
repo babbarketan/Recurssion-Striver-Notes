c38db41438c42235d9ea792c130a2646eebb01ee
Babbar Op <122117993+babbarketan@users.noreply.github.com>
2024-08-22 21:54:52 +0530

    Create Searching in a linked list .cpp

---
+++ Searching in a linked list .cpp
@@ -0,0 +1,57 @@
+int checkifPresent(Node* head,int val){
+    Node* temp = head;
+    while(temp){
+        if(temp->data == val) return 1;
+        temp = temp->next;
+    }
+    return 0;
+}
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+int checkifPresent(Node* head,int val){
+    Node* temp = head;
+    while(temp){
+        if(temp->data == val) return 1;
+        temp = temp->next;
+    }
+    return 0;
+}
+int main(){
+    vector<int> arr = {1,4,2,3};
+    Node* head = convertArr2LL(arr);
+    cout<<head->data<<endl;
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
+*/
